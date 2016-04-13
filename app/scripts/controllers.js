c797ede68f4d28d6c111ca409fc9742233ea0fee
Cezar Mart <czarekmart@gmail.com>
2016-04-12 23:23:27 -0700

    Week 4 lesson 2

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -9,7 +9,15 @@ angular.module('confusionApp')
         //====================================================================
 
         $scope.showDetails = false;
-        $scope.dishes= menuFactory.getDishes();
+
+        $scope.dishes= [];
+        menuFactory.getDishes()
+            .then(
+            function(response) {
+                $scope.dishes = response.data;
+            }
+        );
+
         $scope.select = function(setTab) {
             $scope.tab = setTab;
             if (setTab === 2) {
@@ -71,8 +79,14 @@ angular.module('confusionApp')
     }])
 
     .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
-        var dish= menuFactory.getDish(parseInt($stateParams.id,10));
-        $scope.dish = dish;
+        $scope.dish = {};
+        menuFactory.getDish(parseInt($stateParams.id,10))
+            .then(
+            function(response){
+                $scope.dish = response.data;
+                $scope.showDish=true;
+            }
+        );
     }])
 
     .controller('DishCommentController', ['$scope', function($scope) {
@@ -109,7 +123,17 @@ angular.module('confusionApp')
 
     // implement the IndexController and About Controller here
     .controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory', function($scope, menuFactory, corporateFactory) {
-        $scope.featuredDish = menuFactory.getDish(0);
+
+        $scope.featuredDish = {};
+
+        menuFactory.getDish(0)
+            .then(
+            function(response){
+                $scope.featuredDish = response.data;
+                $scope.showDish = true;
+            }
+        );
+
         $scope.featuredPromotion = menuFactory.getPromotion(0);
         $scope.executiveChef = corporateFactory.getLeader(3);
     }])
